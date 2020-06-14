494f8b1366e82be18b092b6ce79d6843b6dc4735
Shreyas . Potdar <47130139+SHREYAS1188@users.noreply.github.com>
2020-06-14 11:03:35 +0530

    Add files via upload
    backbone file

---
+++ 123.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import cv2
+import numpy as np
+import pyautogui
+
+
+# In[3]:
+
+
+print(pyautogui.size())
+
+
+# In[4]:
+
+
+SCREEN_SIZE = (1920, 1080)
+fourcc = cv2.VideoWriter_fourcc(*"XVID")
+out = cv2.VideoWriter("output.avi", fourcc, 20.0, (SCREEN_SIZE))
+
+
+# In[6]:
+
+
+while True:
+    # make a screenshot
+    img = pyautogui.screenshot()
+    # convert these pixels to a proper numpy array to work with OpenCV
+    frame = np.array(img)
+    # convert colors from BGR to RGB
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    # write the frame
+    out.write(frame)
+    # show the frame
+    cv2.imshow("screenshot", frame)
+    # if the user clicks q, it exits
+    if cv2.waitKey(1) == ord("q"):
+        break
+
+# make sure everything is closed when exited
+cv2.destroyAllWindows()
+out.release()
+
+
+# In[ ]:
+
+
+
+
